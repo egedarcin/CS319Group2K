b77117c93a00c88833ce7623feced65d8b5cdaef
Alptuğ Albayrak <alptuq.albayrak@gmail.com>
2017-11-04 20:41:32 +0300

    Player select menu added

--- Real Path/src/View/PlayerSelect_Controller.java
+++ Real Path/src/View/PlayerSelect_Controller.java
@@ -1,4 +1,22 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PlayerSelect_Controller {
+
+    @FXML
+    private void goBack(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("main_menu.fxml"));
+        stage.setScene(new Scene(root, 1280, 720));
+        stage.show();
+    }
 }
